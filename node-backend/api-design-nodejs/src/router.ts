ec772c1b769fae6c73569c7f431a5a216ab61224
Meargteame <mearegteame99995555@gmail.com>
2025-04-25 16:26:25 +0300

    doing routing

---
+++ node-backend/api-design-nodejs/src/router.ts
@@ -0,0 +1,34 @@
+import { Router } from 'express';
+const router = Router();
+
+/**
+ * Product staff
+ */
+router.get('/product', () => { })
+router.get('/product/:id',()=>{})
+router.put('/product',()=>{})
+router.get('/product',()=>{})
+router.delete('/product/:id',()=>{})
+
+
+
+/**
+ * Update staff
+ */
+
+router.get('/update', () => { })
+router.get('/update/:id',()=>{})
+router.put('/update',()=>{})
+router.get('/update',()=>{})
+router.delete('/update/:id',()=>{})
+
+/**
+ * Update Points staff
+ */
+
+
+router.get('/updatepoint', () => { })
+router.get('/updatepoint/:id',()=>{})
+router.put('/updatepoint',()=>{})
+router.get('/updatepoint',()=>{})
+router.delete('/updatepoint/:id',()=>{})
